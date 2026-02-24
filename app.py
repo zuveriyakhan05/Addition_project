bb56f0ef0e24b887ca97990db50de7f914d94b63
Zuveriya <zuveriyap05@gmail.com>
2026-02-24 10:07:56 +0530

    devops3.0

---
+++ app.py
@@ -0,0 +1,8 @@
+a = 10
+b = 0
+
+def add (x, y):
+    return x+y
+
+result = add(a, b)
+print ("Result is :", result)
---
+++ test_app.py
@@ -0,0 +1,4 @@
+from app import a, b, add
+
+def test_addition():
+    assert add(a, b) == 10
